9641b45f674d2648409ddd8fba79c4cc8a228256
Denys Lytvynenko <den.lytvynenko@temabit.com>
2023-03-17 18:01:27 +0200

    add on drag effect

--- src/components/Upload/index.tsx
+++ src/components/Upload/index.tsx
@@ -1,16 +1,19 @@
-import { DragEvent, FC, useState } from "react";
+import { ChangeEvent, DragEvent, FC, useState } from "react";
 
+import { cn } from "@utils/classNames";
 import { UploadProps } from "./types";
 
-import Typography from "../Typography";
-
-import "./styles.scss";
 import Button from "../Button";
 import FileUploadMessage from "../Message";
+import Typography from "../Typography";
+
+import { ReactComponent as UploadIcon } from "@assets/icons/upload-file.svg";
+
+import "./styles.scss";
 
 const Upload: FC<UploadProps> = () => {
     const [dragActive, setDragActive] = useState<boolean>(false);
-    const [selectedFile, setSelectedFile] = useState();
+    const [file, setFile] = useState<FileList>();
 
     const handleDrag = (e: DragEvent) => {
         e.preventDefault();
@@ -23,25 +26,51 @@ const Upload: FC<UploadProps> = () => {
         }
     };
 
+    const handleDrop = (e: DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        setDragActive(false);
+
+        if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+            setFile(e.dataTransfer.files);
+        }
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (e.target.files && e.target.files[0]) {
+            setFile(e.target.files);
+        }
+    };
+
     return (
         <div className="file-upload-form__wrapper">
             <form
                 className="file-upload-form"
-                onDrag={handleDrag}
+                onDragEnter={handleDrag}
+                onDragLeave={handleDrag}
+                onDragOver={handleDrag}
+                onDrop={handleDrop}
                 onSubmit={e => e.preventDefault()}
             >
                 <input
                     type="file"
                     id="file-upload-form__input"
                     className="file-upload-form__input"
-                    multiple={true}
                     accept="image/png, .jpg"
+                    onChange={handleChange}
                 />
 
                 <label
                     htmlFor="file-upload-form__input"
-                    className="file-upload-form__label"
+                    className={cn(
+                        "file-upload-form__label",
+                        dragActive ? "active" : ""
+                    )}
                 >
+                    <UploadIcon />
+
                     <Typography tag="p">
                         <strong>Drag</strong> here your file or{" "}
                         <strong>Click</strong> here to upload
@@ -50,10 +79,10 @@ const Upload: FC<UploadProps> = () => {
             </form>
 
             <Typography tag="p" className="file-upload-form__filename">
-                {selectedFile ? selectedFile : "No file selected"}
+                {file ? file[0].name : "No file selected"}
             </Typography>
 
-            <Button active>Upload photo</Button>
+            {file && <Button active>Upload photo</Button>}
 
             <FileUploadMessage
                 status="success"
