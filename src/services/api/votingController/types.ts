095121dc826a67c738f41665cbd9cf8978e5de93
Denys Lytvynenko <den.lytvynenko@temabit.com>
2023-03-21 18:33:39 +0200

    feat: add likes button action and like breed api method.

--- src/services/api/votingController/types.ts
+++ src/services/api/votingController/types.ts
@@ -5,3 +5,18 @@ export type RandomBreedType = {
     width: number;
     height: number;
 }[];
+
+export interface VotingBreedDataType {
+    image_id: string;
+    sub_id: string;
+    value: 1 | 10;
+}
+
+export interface VotingBreedResponseType {
+    message: "SUCCESS";
+    id: number;
+    image_id: string;
+    sub_id: string;
+    value: number;
+    country_code: string;
+}
