77bc57041ebe2ecf6b3853cdedf469a87a3e2365
Denys Lytvynenko <den.lytvynenko@temabit.com>
2023-03-28 16:09:56 +0300

    feat: Add filter functionality to gallery route

--- src/routes/gallery/index.tsx
+++ src/routes/gallery/index.tsx
@@ -1,5 +1,11 @@
 import { ChangeEvent, FC, useEffect, useState } from "react";
 
+import { BreedsController } from "@api/breedsController";
+import { ImagesController } from "@api/imagesController";
+import { OptionsType } from "@components/Select/types";
+import { useTiles } from "@hooks/useTiles";
+import { UseTilesDataType } from "@hooks/useTiles/types";
+
 import Button from "@components/Button";
 import ContentWrapper from "@components/ContentWrapper";
 import GalleryGrid from "@components/GalleryGrid";
@@ -13,21 +19,47 @@ import { ReactComponent as RefreshIcon } from "@assets/icons/refresh.svg";
 import { ReactComponent as UploadIcon } from "@assets/icons/upload.svg";
 
 import "./styles.scss";
-import { UseTilesDataType } from "../../hooks/useTiles/types";
-import { useTiles } from "../../hooks/useTiles";
-import { ImagesController } from "../../services/api/imagesController";
-import { OptionsType } from "../../components/Select/types";
+
+const initialOptions: OptionsType = [{ name: "None", value: "" }];
+
+const orderOptions: OptionsType = [
+    { name: "Random", value: "random" },
+    { name: "Asc", value: "asc" },
+    { name: "Desc", value: "desc" },
+];
+
+const typeOptions: OptionsType = [
+    { name: "All", value: "jpg,png,gif" },
+    { name: "Static", value: "jpg,png" },
+    { name: "Animated", value: "gif" },
+];
 
 const limitOptions: OptionsType = [
-    { value: "5", name: "5 items per page" },
-    { value: "10", name: "10 items per page" },
-    { value: "15", name: "15 items per page" },
-    { value: "20", name: "20 items per page" },
+    { name: "5 items per page", value: "5" },
+    { name: "10 items per page", value: "10" },
+    { name: "15 items per page", value: "15" },
+    { name: "20 items per page", value: "20" },
 ];
 
 const Gallery: FC = () => {
+    const [orderValue, setOrderValue] = useState<string>(orderOptions[0].value);
+    const onOrderChange = (event: ChangeEvent<HTMLSelectElement>) =>
+        setOrderValue(event.target.value);
+
+    const [typeValue, setTypeValue] = useState<string>(typeOptions[0].value);
+    const onTypeChange = (event: ChangeEvent<HTMLSelectElement>) =>
+        setTypeValue(event.target.value);
+
+    const [breedsOptions, setBreedsOptions] =
+        useState<OptionsType>(initialOptions);
+    const [breedsValue, setBreedsValue] = useState<string>(
+        breedsOptions[0].value
+    );
+    const onBreedChange = (event: ChangeEvent<HTMLSelectElement>) =>
+        setBreedsValue(event.target.value);
+
     const [limitValue, setLimitValue] = useState<string>(limitOptions[0].value);
-    const onChangeLimit = (event: ChangeEvent<HTMLSelectElement>) =>
+    const onLimitChange = (event: ChangeEvent<HTMLSelectElement>) =>
         setLimitValue(event.target.value);
 
     const [update, setUpdate] = useState<boolean>(false);
@@ -42,19 +74,31 @@ const Gallery: FC = () => {
     useEffect(() => {
         const abortController = new AbortController();
 
+        // Fetch gallery images depending on query parameters
         const getGalleryImages = async () => {
             try {
                 setLoading(true);
+
+                const queryParams = new String().concat(
+                    `&order=${orderValue}`,
+                    `&mime_types=${typeValue}`,
+                    breedsValue ? `&breed_ids=${breedsValue}` : ""
+                );
+
                 const galleryImages =
                     await ImagesController.getInstance().getImages({
                         limit: limitValue,
                         page: "0",
+                        queryParams,
                         signal: abortController.signal,
                     });
 
                 if (galleryImages) {
                     const actualData: UseTilesDataType[] = galleryImages.map(
-                        ({ id, url }) => ({ id, image: url })
+                        ({ id, url }) => ({
+                            id,
+                            image: url,
+                        })
                     );
 
                     setData(actualData);
@@ -79,6 +123,55 @@ const Gallery: FC = () => {
         return () => abortController.abort();
     }, [update]);
 
+    useEffect(() => {
+        const abortController = new AbortController();
+
+        // Fetch breeds select options from the API
+        const getBreedsOptions = async () => {
+            try {
+                const allBreeds =
+                    await BreedsController.getInstance().getBreeds();
+
+                if (allBreeds) {
+                    const breedsOpt = allBreeds
+                        .map(({ id, name }) => ({
+                            name,
+                            value: id,
+                        }))
+                        .filter(item => item.name && item.value);
+
+                    breedsOpt.unshift(initialOptions[0]);
+
+                    // Remove duplicated options
+                    const filteredOpt = breedsOpt.filter(
+                        (item, index, self) =>
+                            index ===
+                            self.findIndex(
+                                t =>
+                                    t?.value === item?.value &&
+                                    t?.name === item?.name
+                            )
+                    );
+
+                    setBreedsOptions(filteredOpt);
+                } else {
+                    setBreedsOptions(initialOptions);
+                }
+            } catch (error) {
+                if (abortController.signal.aborted) {
+                    console.log("Request canceled by the user");
+                } else {
+                    console.error("Get breeds options error: ", error);
+                    setBreedsOptions(initialOptions);
+                }
+            }
+        };
+
+        getBreedsOptions();
+
+        return () => abortController.abort();
+    }, []);
+
     const tiles = useTiles({ data, component: GalleryRouteTile });
 
     return (
@@ -100,27 +193,27 @@ const Gallery: FC = () => {
                         name="order"
                         title="order"
                         label="order"
-                        options={[{ value: "Some value", name: "name" }]}
-                        value="name"
-                        onChange={() => {}}
+                        options={orderOptions}
+                        value={orderValue}
+                        onChange={onOrderChange}
                     />
 
                     <Select
                         name="type"
                         title="type"
                         label="type"
-                        options={[{ value: "Some value", name: "name" }]}
-                        value="name"
-                        onChange={() => {}}
+                        options={typeOptions}
+                        value={typeValue}
+                        onChange={onTypeChange}
                     />
 
                     <Select
                         name="breed"
                         title="breed"
                         label="breed"
-                        options={[{ value: "Some value", name: "name" }]}
-                        value="name"
-                        onChange={() => {}}
+                        options={breedsOptions}
+                        value={breedsValue}
+                        onChange={onBreedChange}
                     />
 
                     <div className="gallery__filters-group">
@@ -130,7 +223,7 @@ const Gallery: FC = () => {
                             label="limit"
                             options={limitOptions}
                             value={limitValue}
-                            onChange={onChangeLimit}
+                            onChange={onLimitChange}
                         />
 
                         <Button
