cf44724707f1ee1910f44d7b4cb3757bb195d78e
Denys Lytvynenko <denys.m.lytvynenko@gmail.com>
2023-05-17 07:09:23 +0300

    Fix: Redirect to index.html on page reboot

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,11 @@
-const root = "/cats-app";
-
 export const routes = {
-    root: `${root}`,
-    home: `${root}/home`,
-    voting: `${root}/voting`,
-    breeds: `${root}/breeds`,
-    gallery: `${root}/gallery`,
-    likes: `${root}/likes`,
-    dislikes: `${root}/dislikes`,
-    favourites: `${root}/favourites`,
-    search: `${root}/search`,
+    root: "/",
+    home: "/home",
+    voting: "/voting",
+    breeds: "/breeds",
+    gallery: "/gallery",
+    likes: "/likes",
+    dislikes: "/dislikes",
+    favourites: "/favourites",
+    search: "/search",
 };
