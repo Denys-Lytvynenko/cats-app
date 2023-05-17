c7cfe15cc26d5c6ba5c634fc340a48c02a62c084
Denys Lytvynenko <denys.m.lytvynenko@gmail.com>
2023-05-17 03:01:58 +0300

    Fix: Change base url to slash in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/cats-app/",
+    base: "/",
     plugins: [react(), svgr()],
     resolve: {
         alias: {
